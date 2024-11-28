25c8553e8b5621248e5c00657e3f958418bcff58
emred007 <emreduman701@gmail.com>
2024-11-28 14:24:05 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <renkDeğiştirenYazı></renkDeğiştirenYazı>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import renkDeğiştirenYazı from './renkDeğiştirenYazı.vue'; 
+</script>
---
+++ renkDeğiştirenYazı.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1 :style="{ color: yazıRengi }  ">Hello World</h1>
+      <button @click="renkDeğiştir" class="bg-red mt-1">Rengi Değiştir</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+ 
+  const karakterler = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+  
+  function rasgeleRenkKodu() {
+    let renkKodu = '#';
+    for (let i = 0; i < 6; i++) {
+      renkKodu += karakterler[Math.floor(Math.random() * karakterler.length)];
+    }
+    return renkKodu;
+  }
+ 
+  const yazıRengi = ref('#000');
+  
+
+  function renkDeğiştir() {
+    yazıRengi.value = rasgeleRenkKodu();
+  }
+  </script>
+
+
+
+  
